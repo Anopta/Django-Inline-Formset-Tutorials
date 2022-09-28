129de5a1813fe7cd8d96bf288973c126cc776d00
Anopta <66787374+Anopta@users.noreply.github.com>
2022-09-28 17:03:00 +0900

    update

--- django_inline_formset/products/models.py
+++ django_inline_formset/products/models.py
@@ -26,7 +26,7 @@ class Variant(models.Model):
         Product, on_delete=models.CASCADE
         )
     size = models.CharField(max_length=100)
-    quantity = models.PositiveIntegerField(default=1)
+    quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=12, decimal_places=2)
 
     def __str__(self):
